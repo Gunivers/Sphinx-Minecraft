26823a3d7918be32d9a878d9fd2628f6b49e3cb2
Théo Giraudet <theo.giraudet@orange.fr>
2024-12-04 21:25:23 +0100

    Initial commit

---
+++ sphinx_minecraft/__init__.py
@@ -0,0 +1,41 @@
+from enum import Enum
+import os
+from pathlib import Path
+from sphinx.util import logging
+from sphinx.application import Sphinx
+from sphinx_treeview.decorator import DecoratorType, imagesToDecoratorIcons
+
+logger = logging.getLogger(__name__)
+
+__version__ = "0.1.0"
+
+class DectoratorRegistry(Enum):
+    MCDIR = "mcdir"
+    NBT = "nbt"
+    
+def add_static_path(app):
+    static_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "_static"))
+    app.config.html_static_path.append(static_path)
+    
+
+def setup(app: Sphinx):
+    
+    app.setup_extension('sphinx-treeview')
+    app.add_css_file('minecraft_sphinx.css')
+    
+    decorators = []
+    
+    for decorator in DectoratorRegistry:
+        dir_type = DecoratorType(decorator.value, imagesToDecoratorIcons(Path(__file__).parent / '_static' / 'images' / decorator.value, f"smc/images/treeview_icons/{decorator.value}"))
+        decorators.append(dir_type)
+        logger.verbose(f"Tree decorator '{decorator.value}' added.")
+    
+    app.config.tree_decorators.append(decorators)
+    app.connect("builder-inited", add_static_path)
+    
+    return {
+        "version": __version__,
+    }
+    
+    
+    
